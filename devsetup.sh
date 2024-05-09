ae1194c439ca9a946d0c28b473f616a189fd584d
Declan Middleton <declanredd1722@gmail.com>
2024-05-09 14:13:02 +1200

    Add files via upload

---
+++ devsetup.sh
@@ -0,0 +1,24 @@
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install brave-browser
+brew install visual-studio-code
+brew install figma
+brew install discord 
+brew install firefox
+brew install insomnia
+brew install iterm2
+brew install notion
+brew install notion-calendar
+brew install vanilla
+brew install tableplus
+brew install zoom
+brew install pnpm
+brew install curl
+brew install node
+brew install openvpn
+brew install mysql
+brew install wget
+brew install php
+brew install nmap
+brew install google-chrome
+echo "Done installing! Enjoy coding!!"
